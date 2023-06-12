4e66213e75f1dae592df82e2c26b101612c8c43b
Camila Aylen Lopez <115021067+CamilaAylenLopez@users.noreply.github.com>
2023-06-12 08:41:44 -0300

    a

--- src/componentes/juego.jsx
+++ src/componentes/juego.jsx
@@ -2,12 +2,26 @@ import './juego.css'
 import React, { Fragment, useState } from "react"
 
 const juego = (props) => {
+    if(respueta == bandera && intentos == 1){
+        puntos = puntos + variable;
+    }
+    else if(respueta == bandera && intentos == 2){
+        puntos = puntos + variableDos;
+    }
+    else if(respueta == bandera && intentos == 3){
+        puntos = puntos + variableTres;
+    }
+    else{
+        juegoTerminado = true;
+    }
+
     return(
         <div>
             <h1>Jueguito de Banderas</h1>
-            <h3>Puntos: ${puntos}</h3>
-            <img></img>
+            <img href="${bandera}"/>
             <input type="text" name="pais" className="u-full-width" placeholder="Pais"/>
+            <button type="submit" className="u-full-width button-primary">Submit</button>
+            <h3>Puntos: ${puntos}</h3>
         </div>
     )
 }
