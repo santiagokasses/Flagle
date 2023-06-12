edb9348cf5b9ff84827a57efeb0218c72a84daa0
Camila Aylen Lopez <115021067+CamilaAylenLopez@users.noreply.github.com>
2023-06-12 09:01:22 -0300

    aaaaaa

--- src/componentes/juego.jsx
+++ src/componentes/juego.jsx
@@ -2,26 +2,46 @@ import './juego.css'
 import React, { Fragment, useState } from "react"
 
 const juego = (props) => {
-    if(respueta == bandera && intentos == 1){
+    const variable = 20;
+    const variableDos = 15;
+    const variableTres = 10;
+    let cantAyudas = 3;
+    let puntos = 0;
+    const puntosDeAyuda = 5;
+    let vidas = 3;
+    let juegoTerminado = false;
+    let ayudaUsada = false;
+    
+    //eliminar
+    let bandera = "Argentina";
+    let respueta = "Argentina";
+    let cantIntentos = 2;
+
+    if(respueta == bandera && cantIntentos == 1){
         puntos = puntos + variable;
     }
-    else if(respueta == bandera && intentos == 2){
+    else if(respueta == bandera && cantIntentos == 2){
         puntos = puntos + variableDos;
     }
-    else if(respueta == bandera && intentos == 3){
+    else if(respueta == bandera && cantIntentos == 3){
         puntos = puntos + variableTres;
     }
     else{
         juegoTerminado = true;
     }
+    if(ayudaUsada){
+        puntos = puntos - puntosDeAyuda;
+    }
 
     return(
         <div>
             <h1>Jueguito de Banderas</h1>
-            <img href="${bandera}"/>
+            <h3>Vidas: {vidas}</h3>
+            <img href = {bandera}/>
             <input type="text" name="pais" className="u-full-width" placeholder="Pais"/>
+            <button type="" >Ayuda {cantAyudas}</button>
             <button type="submit" className="u-full-width button-primary">Submit</button>
-            <h3>Puntos: ${puntos}</h3>
+            <h3>Puntos: {puntos}</h3>
         </div>
     )
 }
