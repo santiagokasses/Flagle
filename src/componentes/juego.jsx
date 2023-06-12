557b09e079f80dad2227745fa4f3ac443c7e03f5
Camila Aylen Lopez <115021067+CamilaAylenLopez@users.noreply.github.com>
2023-06-12 08:34:54 -0300

    i

--- src/componentes/juego.jsx
+++ src/componentes/juego.jsx
@@ -1,22 +1,6 @@
 import './juego.css'
 import React, { Fragment, useState } from "react"
 
-
-console.log("Hago consulta - axios");
-(async () => { 
-    console.log("Hago async consulta - axios")
-    await axios({
-        method: 'get',
-        url: 'https://countriesnow.space/api/v0.1/countries/flag/images',
-    })
-    .then(res => {
-        console.log("res", res)
-        valores = document.getElementById("valores");
-    })
-    .catch(err => console.error("error", err))
-    console.log("Fin async consulta - axios")
-})();
-
 const juego = (props) => {
     return(
         <div>
@@ -28,4 +12,4 @@ const juego = (props) => {
     )
 }
 
-export default Juego
+export default juego
