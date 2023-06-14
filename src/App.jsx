25710be77f0b968553102d6ed1462f5b7908fbe6
santiagokasses <101513911+santiagokasses@users.noreply.github.com>
2023-06-13 23:03:22 -0300

    funciona

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Juego from './componentes/juego'
+import Juego from './componentes/Juego'
 
 function App() {
   return (
--- src/componentes/juego.jsx
+++ src/componentes/juego.jsx
@@ -1,34 +1,62 @@
-import './juego.css'
-import React, { Fragment, useState } from "react"
+import './Juego.css'
+import React, { Fragment, useState, useEffect } from "react"
 
-const juego = (props) => {
-    const variable = 20;
-    const variableDos = 15;
-    const variableTres = 10;
-    let cantAyudas = 3;
-    let puntos = 0;
-    const puntosDeAyuda = 5;
-    let vidas = 3;
-    let juegoTerminado = false;
-    let ayudaUsada = false;
-    
-    //eliminar
-    let bandera = "Argentina";
-    let respueta = "Argentina";
-    let cantIntentos = 2;
+const Juego = (props) => {
+    const [Bandera, setBandera] = useState([{
+        bandera: null,
+        nombre: null,
+    }])
+    const [Abandera, setABandera] = useState(0)
+    const [puntos, setPuntos] = useState(0)
 
+    // const variable = 20;
+    // const variableDos = 15;
+    // const variableTres = 10;
+    // let cantAyudas = 3;
+    // const puntosDeAyuda = 5;
+    // let vidas = 3;
+    // let juegoTerminado = false;
+    // let ayudaUsada = false;
 
-    const [Bandera, setBanderda] = useState('')
+    // //eliminar
+    // let respueta = "Argentina";
+    // let cantIntentos = 2;
 
-    console.log("Hago consulta - fetch")
-    fetch("https://countriesnow.space/api/v0.1/countries/flag/images")
-        .then(res => res.json())
-        .then(res => {
-            Bandera = res.data[1]
-            console.log(res.data[1])
-        })
-        .catch(err => console.error("error", err))
-    console.log("Fin consulta - fetch")
+    useEffect(() => {
+        const obtenerAbanderas = () => {
+            fetch("https://countriesnow.space/api/v0.1/countries/flag/images")
+                .then(res => res.json())
+                .then(res => {
+                    const updatedBandera = res.data.map(e => ({
+                        bandera: e.flag,
+                        nombre: e.name.toUpperCase()
+                    }))
+                    setBandera(updatedBandera)
+                })
+        }
+        obtenerAbanderas()
+    }, [])
+    useEffect(()=>{
+        setABandera(Math.floor(Math.random() * Bandera.length))
+        console.log(Abandera)
+    }, [Bandera])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (Bandera[Abandera].nombre == document.getElementsByName("pais")[0].value.toUpperCase()) {
+            document.getElementsByName("pais")[0].value = ''
+            setABandera(Math.floor(Math.random() * Bandera.length))
+            setPuntos((e) => e + 10)
+            console.log("correcto")
+            console.log(document.getElementsByName("pais")[0].value)
+        }
+        else {
+            document.getElementsByName("pais")[0].value = ''
+            setPuntos((e) => e -1)
+            console.log("incorrecto")
+            console.log(document.getElementsByName("pais")[0].value)
+        }
+    }
     // if (respueta == bandera && cantIntentos == 1) {
     //     puntos = puntos + variable;
     // }
@@ -41,19 +69,22 @@ const juego = (props) => {
     // else {
     //     juegoTerminado = true;
     // }
-    if(ayudaUsada){
-        puntos = puntos - puntosDeAyuda;
-    }
-
+    // if (ayudaUsada) {
+    //     puntos = puntos - puntosDeAyuda;
+    // }
     return (
-        <div>
+        <>
             <h1>Jueguito de Banderas</h1>
-            <img href="${bandera}"/>
-            <input type="text" name="pais" className="u-full-width" placeholder="Pais"/>
-            <button type="submit" className="u-full-width button-primary">Submit</button>
-            <h3>Puntos: ${puntos}</h3>
-        </div>
+            <h2>¿De que pais es esta bandera?</h2>
+            <div className="banderita" ></div>
+            <form onSubmit={handleSubmit}>
+                <div><img src={Bandera[Abandera].bandera}/></div>
+                <input type="text" name="pais" className="u-full-width" placeholder={Bandera[Abandera].nombre} />
+                <button type="submit" className="u-full-width button-primary">Submit</button>
+                <h3>Puntos: {puntos}</h3>
+            </form>
+        </>
     )
 }
 
-export default juego
+export default Juego
