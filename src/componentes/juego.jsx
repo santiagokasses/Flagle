a5de6706b97ef5c23e244feb2c777a353b2ed4d4
santiagokasses <101513911+santiagokasses@users.noreply.github.com>
2023-07-02 19:40:43 -0300

    Update juego.jsx

--- src/componentes/juego.jsx
+++ src/componentes/juego.jsx
@@ -6,9 +6,9 @@ const Juego = (props) => {
         bandera: null,
         nombre: null,
     }])
-    const [Abandera, setABandera] = useState(0)
+    const [BanderaActual, setBanderaActual] = useState(0)
     const [puntos, setPuntos] = useState(0)
-    const [timer, setTimer] = useState(0)
+    //const [timer, setTimer] = useState(0)
     useEffect(() => {
         const obtenerAbanderas = async () => {
           try {
@@ -19,7 +19,7 @@ const Juego = (props) => {
               nombre: e.name.toUpperCase()
             }))
             setBandera(updatedBandera)
-            setABandera(Math.floor(Math.random() * updatedBandera.length))
+            setBanderaActual(Math.floor(Math.random() * updatedBandera.length))
           } catch (error) {
             console.error(error)
           }
@@ -29,18 +29,14 @@ const Juego = (props) => {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        if (Bandera[Abandera].nombre == document.getElementsByName("pais")[0].value.toUpperCase()) {
+        if (Bandera[BanderaActual].nombre === document.getElementsByName("pais")[0].value.toUpperCase()) {
             document.getElementsByName("pais")[0].value = ''
-            setABandera(Math.floor(Math.random() * Bandera.length))
-            setPuntos((e) => e + 10)
-            console.log("correcto")
-            console.log(document.getElementsByName("pais")[0].value)
+            setBanderaActual(Math.floor(Math.random() * Bandera.length))
+            setPuntos(puntos + 10)
         }
         else {
             document.getElementsByName("pais")[0].value = ''
-            setPuntos((e) => e -1)
-            console.log("incorrecto")
-            console.log(document.getElementsByName("pais")[0].value)
+            setPuntos(puntos - 1)
         }
     }
     
@@ -50,8 +46,8 @@ const Juego = (props) => {
             <h2>¿De que pais es esta bandera?</h2>
             <div className="banderita" ></div>
             <form onSubmit={handleSubmit}>
-                <div><img src={Bandera[Abandera].bandera}/></div>
-                <input type="text" name="pais" className="u-full-width" placeholder={Bandera[Abandera].nombre} />
+                <div><img alt='' src={Bandera[BanderaActual].bandera}/></div>
+                <input type="text" name="pais" className="u-full-width" /*placeholder={Bandera[BanderaActual].nombre}*/ />
                 <button type="submit" className="u-full-width button-primary">Submit</button>
                 <h3>Puntos: {puntos}</h3>
             </form>
